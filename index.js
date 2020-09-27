93e1377b45a28a7038b44e2de6e38c23e967be79
Rioshanohan <46582183+Rioshanohan@users.noreply.github.com>
2020-09-27 15:55:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const roll = (sides, mult = 1, mod = 0) => {
   console.log(mult);
   console.log(mod);
 }
+
+const getRoll = () => {
+  let sid = document.getElementById("sides").value;
+  let mul = document.getElementById("mult").value;
+  let md = document.getElementById("mod").value;
+  roll(sid, mul, md);
+}
